6f30b05817ca04e053e414cd19411d566dc86e61
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2022-12-06 20:06:15 +0100

    adds solution for day 6

---
+++ src/Day06.kt
@@ -0,0 +1,26 @@
+private fun String.findStartIndexOfMarker(markerLength: Int): Int = this
+    .windowed(size = markerLength, step = 1)
+    .indexOfFirst { sequence -> sequence.toSet().size == markerLength }
+
+private fun part1(input: String): Int {
+    val markerLength = 4
+
+    return input.findStartIndexOfMarker(markerLength) + markerLength
+}
+
+private fun part2(input: String): Int {
+    val markerLength = 14
+
+    return input.findStartIndexOfMarker(markerLength) + markerLength
+}
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test").single()
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 26)
+
+    val input = readInput("Day06").single()
+    println(part1(input))
+    println(part2(input))
+}
