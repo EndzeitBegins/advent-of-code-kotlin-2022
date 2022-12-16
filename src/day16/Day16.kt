204dcb8bb47434822efea6dba91e45e8f471b1ab
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2022-12-17 00:44:42 +0100

    adds solution for day 16 part 2

--- src/day16/Day16.kt
+++ src/day16/Day16.kt
@@ -10,13 +10,6 @@ private data class Valve(
 
 private typealias Valves = Map<String, Valve>
 
-private data class State(
-    val position: Valve,
-    val openValves: Set<Valve>,
-    val totalFlowRate: Long,
-    val releasedPressure: Long,
-)
-
 private fun List<String>.toValves(): Valves {
     val regex = """Valve (\w+) has flow rate=(\d+);.*valves? (\w+(, \w+)*)""".toRegex()
 
@@ -59,68 +52,119 @@ private fun Valves.findShortestRoutes(): Map<String, Map<String, Int>> {
     return shortestRoutes
 }
 
-private data class Option(
+private data class PossibleMove(
     val position: String,
     val valvesOpened: Set<String>,
     val releasedPressureAtEnd: Long,
     val timeLeft: Int,
 )
 
-private fun Option.availableOptions(valves: Valves, shortestRoutes: Map<String, Map<String, Int>>): List<Option> {
-    val option = this
+private fun findPossibleTargets(
+    shortestRoutes: Map<String, Map<String, Int>>,
+    valves: Valves,
+    position: String,
+    timeLeft: Int,
+    valvesOpened: Set<String>
+) = shortestRoutes.getValue(position)
+    .filterNot { (valveName, moveCost) ->
+        val possibleTargetValve = valves.getValue(valveName)
+        val cannotOpen = timeLeft < (moveCost + 2)
 
-    val possibleTargets = shortestRoutes.getValue(option.position)
-        .filterNot { (valveName, moveCost) ->
-            val possibleTargetValve = valves.getValue(valveName)
-            val cannotOpen = option.timeLeft < (moveCost + 2)
+        valveName in valvesOpened || possibleTargetValve.flowRate == 0L || cannotOpen
+    }
 
-            valveName in option.valvesOpened || possibleTargetValve.flowRate == 0L ||  cannotOpen
-        }
+private fun PossibleMove.availableMoves(
+    valves: Valves,
+    shortestRoutes: Map<String, Map<String, Int>>
+): List<PossibleMove> {
+    val move = this
 
-    if (option.timeLeft <= 0 || possibleTargets.isEmpty()) {
-        return listOf(option)
+    val possibleTargets = findPossibleTargets(
+        shortestRoutes = shortestRoutes,
+        valves = valves,
+        position = move.position,
+        timeLeft = move.timeLeft,
+        valvesOpened = move.valvesOpened
+    )
+
+    if (move.timeLeft <= 0 || possibleTargets.isEmpty()) {
+        return listOf(move)
     }
 
     return possibleTargets.flatMap { (targetValveName, moveCost) ->
         val targetValve = valves.getValue(targetValveName)
         val openCost = moveCost + 1
-        val releasedPressureFromValveAtEnd = (option.timeLeft - openCost) * targetValve.flowRate
+        val releasedPressureFromValveAtEnd = (move.timeLeft - openCost) * targetValve.flowRate
 
-        Option(
+        PossibleMove(
             position = targetValveName,
-            valvesOpened = option.valvesOpened + targetValveName,
-            releasedPressureAtEnd = option.releasedPressureAtEnd + releasedPressureFromValveAtEnd,
-            timeLeft = option.timeLeft -  openCost
-        ).availableOptions(valves, shortestRoutes)
-    }
+            valvesOpened = move.valvesOpened + targetValveName,
+            releasedPressureAtEnd = move.releasedPressureAtEnd + releasedPressureFromValveAtEnd,
+            timeLeft = move.timeLeft - openCost
+        ).availableMoves(valves, shortestRoutes)
+    } + move
 }
 
 private fun part1(input: List<String>): Long {
     val valves = input.toValves()
     val shortestRoutes = valves.findShortestRoutes()
+        .filterKeys { valveName -> valveName == "AA" || valves.getValue(valveName).flowRate > 0 }
 
-    val initialOption = Option(
+    val initialMove = PossibleMove(
         position = "AA",
         valvesOpened = emptySet(),
         releasedPressureAtEnd = 0L,
         timeLeft = 30,
     )
 
-    val options: List<Option> = listOf(initialOption)
-        .flatMap { option -> option.availableOptions(valves, shortestRoutes) }
+    val possibleMoves: List<PossibleMove> = listOf(initialMove)
+        .flatMap { move -> move.availableMoves(valves, shortestRoutes) }
 
-    return options.maxOf { option -> option.releasedPressureAtEnd }
+    return possibleMoves.maxOf { move -> move.releasedPressureAtEnd }
 }
 
 private fun part2(input: List<String>): Long {
-    return input.size.toLong()
+    val valves = input.toValves()
+    val shortestRoutes = valves.findShortestRoutes()
+        .filterKeys { valveName -> valveName == "AA" || valves.getValue(valveName).flowRate > 0 }
+
+    val initialMove = PossibleMove(
+        position = "AA",
+        valvesOpened = emptySet(),
+        releasedPressureAtEnd = 0L,
+        timeLeft = 26,
+    )
+
+    val possibleMoves: List<PossibleMove> = listOf(initialMove)
+        .flatMap { move -> move.availableMoves(valves, shortestRoutes) }
+
+    var maximumPressureRelease = 0L
+    for (i in possibleMoves.indices) {
+        val myMove = possibleMoves[i]
+        val myOpenedValves = myMove.valvesOpened
+        val myPressureRelease = myMove.releasedPressureAtEnd
+
+        for (j in i..possibleMoves.lastIndex) {
+            val elephantMove = possibleMoves[j]
+            val elephantOpenedValves = elephantMove.valvesOpened
+            val elephantPressureRelease = elephantMove.releasedPressureAtEnd
+            val combinedPressureRelease = myPressureRelease + elephantPressureRelease
+
+            if (maximumPressureRelease < combinedPressureRelease &&
+                myOpenedValves.none { myValve -> myValve in elephantOpenedValves }) {
+                maximumPressureRelease = combinedPressureRelease
+            }
+        }
+    }
+
+    return maximumPressureRelease
 }
 
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readTestInput("Day16")
     check(part1(testInput) == 1651L)
-//    check(part2(testInput) == 1707L)
+    check(part2(testInput) == 1707L)
 
     val input = readInput("Day16")
     println(part1(input))
