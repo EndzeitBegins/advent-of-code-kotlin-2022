f0fab0fa9bb91ec4da45f7af31642c8ff5625058
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2022-12-25 11:09:12 +0100

    add solution for day 25 part 1

--- src/day25/Day25.kt
+++ src/day25/Day25.kt
@@ -2,9 +2,71 @@ package day25
 
 import readInput
 import readTestInput
+import kotlin.math.pow
 
-private fun part1(input: List<String>): Int {
-    return input.size
+private val snafuPlaceValues = mutableListOf<Long>()
+
+private fun snafuToDecimal(snafu: String): Long {
+    var value = 0L
+
+    for (exponent in snafu.indices) {
+        val index = snafu.lastIndex - exponent
+
+        val digitValue = when(snafu[index]) {
+            '0' -> 0
+            '1' -> 1
+            '2' -> 2
+            '-' -> -1
+            '=' -> -2
+            else -> error("Unsupported digit '${snafu[index]}' for snafu format!")
+        }
+        
+        if (snafuPlaceValues.lastIndex < exponent) {
+            snafuPlaceValues.add(5.0.pow(exponent).toLong())
+        }
+
+        value += digitValue * snafuPlaceValues[exponent]
+    }
+
+    return value
+}
+
+private fun decimalToSnafu(decimal: Long): String {
+    val quinary = decimal.toString(radix = 5)
+    
+    val snafu = buildString {
+        var remainder: Int? = null
+
+        for (index in quinary.indices.reversed()) {
+            val xxx = quinary[index].digitToInt() + (remainder ?: 0)
+            val digit = xxx % 5
+            remainder = xxx / 5
+
+            val snafuRepresentation = when(digit) {
+                0 -> "0"
+                1 -> "1"
+                2 -> "2"
+                3 -> "1="
+                4 -> "1-"
+                else -> error("Cannot represent $digit as snafu digit!")
+            }
+
+            append(snafuRepresentation.last())
+
+            if (snafuRepresentation.length > 1) {
+                remainder = snafuToDecimal(snafuRepresentation.dropLast(1)).toInt()
+            }
+        }
+    }.reversed()
+    
+    return snafu
+}
+
+private fun part1(input: List<String>): String {
+    val fuelAmount = input
+        .sumOf { snafuNumber -> snafuToDecimal(snafuNumber) }
+
+    return decimalToSnafu(fuelAmount)
 }
 
 private fun part2(input: List<String>): Int {
@@ -14,7 +76,7 @@ private fun part2(input: List<String>): Int {
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readTestInput("Day25")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == "2=-1=0")
 //    check(part2(testInput) == 1)
 
     val input = readInput("Day25")
